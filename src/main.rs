3b13f1865d67ff98baecd6e3e195cf4f2a12da70
Erick Tryzelaar <erick.tryzelaar@gmail.com>
2015-12-24 14:36:06 -0500

    simplify main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,29 +2,19 @@
 #![plugin(stateful)]
 #![allow(unused_variables)]
 
-fn increment(x: usize) -> usize { x + 1 }
-
 #[state_machine]
-fn yield_(mut i: usize, then: usize, else_: usize) -> usize {
+fn yield_(i: usize, then: usize, else_: usize) -> usize {
     loop {
         if i < 10 {
             break;
         } else {
-            i += 1;
             return else_;
-            i += 2;
         };
     };
 }
 
 fn main() {
-    for value in yield_(true, 0, 1).take(20) {
-        println!("yield_: {:?}", value);
-    }
-
-    println!("");
-
-    for value in yield_(false, 0, 1).take(20) {
+    for value in yield_(0, 0, 1).take(20) {
         println!("yield_: {:?}", value);
     }
 }
