2553aaff88fb6e2beb06ad04563733c0a036b89c
Noam Baraz <n0987b@gmail.com>
2016-07-13 18:28:17 +0300

    Move preparation of the ast to a new file
    Prepare the ast (desugar it, assign node ids) before doing anything complicated.

--- src/mar/build/expr.rs
+++ src/mar/build/expr.rs
@@ -1,4 +1,3 @@
-use aster::AstBuilder;
 use mar::build::Builder;
 use mar::build::scope::LoopScope;
 use mar::repr::*;
@@ -74,7 +73,6 @@ impl<'a, 'b: 'a> Builder<'a, 'b> {
             }
             ExprKind::Match(ref discriminant, ref arms) => {
                 self.match_expr(extent, expr.span, block, discriminant.clone(), arms)
-
             }
             ExprKind::Loop(ref body, label) => {
                 self.expr_loop(extent, block, None, body, label)
@@ -82,166 +80,10 @@ impl<'a, 'b: 'a> Builder<'a, 'b> {
             ExprKind::While(ref cond_expr, ref body, label) => {
                 self.expr_loop(extent, block, Some(cond_expr), body, label)
             }
-            ExprKind::ForLoop(ref pat, ref expr, ref loop_block, label) => {
-                // Desugar a for loop into:
-                //
-                // {
-                //     let mut iter = ::std::iter::IntoIterator::into_iter($expr);
-                //     'label: loop {
-                //         match iter.next() {
-                //             ::std::option::Option::Some($pat) => $loop_block,
-                //             ::std::option::Option::None => break,
-                //         }
-                //     }
-                // }
-                let builder = AstBuilder::new().span(expr.span);
-
-                // ::std::iter::IntoIterator::into_iter($expr)
-                let into_iter = builder.expr().call()
-                    .path()
-                        .global()
-                        .ids(&["std", "iter", "IntoIterator", "into_iter"])
-                        .build()
-                    .with_arg(expr.clone())
-                    .build();
-
-                // iter.next()
-                let iter_next = builder.expr().method_call("next")
-                    .id("__stateful_iter")
-                    .build();
-
-                // ::std::option::Option::Some($pat)
-                let some_pat = builder.pat().enum_()
-                    .global().ids(&["std", "option", "Option", "Some"]).build()
-                    .pat().build(pat.clone())
-                    .build();
-
-                // $some_pat => $loop_block
-                let some_arm = builder.arm()
-                    .with_pat(some_pat)
-                    .body().build_block(loop_block.clone());
-
-                // ::std::option::Option::None
-                let none_pat = builder.pat().path()
-                    .global().ids(&["std", "option", "Option", "None"]).build();
-
-                // $none_pat => break,
-                let none_arm = builder.arm()
-                    .with_pat(none_pat)
-                    .body().break_();
-
-                // match $iter_next() {
-                //     Some($pat) => $block,
-                //     None => break,
-                // }
-                let match_expr = builder.expr()
-                    .match_().build(iter_next)
-                    .with_arm(some_arm)
-                    .with_arm(none_arm)
-                    .build();
-
-                // `loop { $match_expr; };`
-                let mut loop_builder = builder.expr().loop_();
-
-                if let Some(label) = label {
-                    loop_builder = loop_builder.label(label.node);
-                }
-
-                let loop_ = loop_builder.block()
-                    .stmt().build_expr(match_expr)
-                    .build();
-
-                // `let mut iter = $into_iter;`
-                let iter = builder.stmt()
-                    .let_().mut_id("__stateful_iter")
-                    .build_expr(into_iter);
-
-                // {
-                //     $into_iter;
-                //     $loop;
-                // }
-                let expr = builder.expr().block()
-                    .with_stmt(iter)
-                    .stmt().build_expr(loop_)
-                    .build();
-
-                self.expr(extent, block, &expr)
-            }
-            ExprKind::IfLet(ref pat, ref expr, ref then_block, ref else_block) => {
-                // Desugar an if-let:
-                //
-                // match $expr {
-                //     $pat => $then_block,
-                //     _ => $else_block,
-                // }
-                let builder = AstBuilder::new().span(expr.span);
-
-                // $then_pat => $then_block
-                let then_arm = builder.arm()
-                    .with_pat(pat.clone())
-                    .body().build_block(then_block.clone());
-
-                // _ => $else_block
-                let else_arm = match *else_block {
-                    Some(ref else_block) => builder.arm().pat().wild().body().build(else_block.clone()),
-                    None => builder.arm().pat().wild().body().unit(),
-                };
-
-                // match $iter_next() {
-                //     $pat => $then_block,
-                //     _ => #else_block,
-                // }
-                let expr = builder.expr()
-                    .match_().build(expr.clone())
-                    .with_arm(then_arm)
-                    .with_arm(else_arm)
-                    .build();
-
-                self.expr(extent, block, &expr)
-            }
-            ExprKind::WhileLet(ref pat, ref expr, ref then_block, label) => {
-                // Desugar an while-let:
-                //
-                // 'label: loop {
-                //     match $expr {
-                //         $pat => $body_block,
-                //         _ => break,
-                //     }
-                // }
-                let builder = AstBuilder::new().span(expr.span);
-
-                // $pat => $then_block
-                let then_arm = builder.arm()
-                    .with_pat(pat.clone())
-                    .body().build_block(then_block.clone());
-
-                // _ => break
-                let else_arm = builder.arm()
-                    .pat().wild()
-                    .body().break_();
-
-                // match $expr {
-                //     $then_arm,
-                //     $else_arm,
-                // }
-                let match_expr = builder.expr()
-                    .match_().build(expr.clone())
-                    .with_arm(then_arm)
-                    .with_arm(else_arm)
-                    .build();
-
-                // `'$label: loop { $match_expr; };`
-                let mut loop_builder = builder.expr().loop_();
-
-                if let Some(label) = label {
-                    loop_builder = loop_builder.label(label.node);
-                }
-
-                let loop_expr = loop_builder.block()
-                    .stmt().build_expr(match_expr)
-                    .build();
-
-                self.expr(extent, block, &loop_expr)
+            ExprKind::ForLoop(..) |
+            ExprKind::IfLet(..)   |
+            ExprKind::WhileLet(..) => {
+                panic!("{:?} Should never reach this point - `preapare` should have desugared this.", expr);
             }
             _ => {
                 self.cx.span_bug(expr.span,
---
+++ src/mar/build/simplify.rs
@@ -0,0 +1,275 @@
+use aster::AstBuilder;
+
+use syntax::ast::{self, Expr, ExprKind, Pat,
+                  Block, SpannedIdent, Item};
+use syntax::fold::{self, Folder};
+use syntax::ptr::P;
+use syntax::codemap::{respan, Span};
+
+use mar::build::transition::ContainsTransition;
+
+pub fn simplify_item(item: P<Item>) -> Item {
+    let mut expander = Expander::new();
+    let mut assigner = Assigner { next_node_id: 1 };
+
+    let expanded = expander.fold_item_simple(item.unwrap());
+    assigner.fold_item_simple(expanded)
+}
+
+pub struct Assigner {
+    pub next_node_id: ast::NodeId,
+}
+
+impl Folder for Assigner {
+    fn new_id(&mut self, old_id: ast::NodeId) -> ast::NodeId {
+        assert_eq!(old_id, ast::DUMMY_NODE_ID);
+
+        let node_id = self.next_node_id;
+
+        let next_node_id = match self.next_node_id.checked_add(1) {
+            Some(next_node_id) => next_node_id,
+            None => { panic!("ran out of node ids!") }
+        };
+        self.next_node_id = next_node_id;
+
+        node_id
+    }
+
+    fn fold_mac(&mut self, mac: ast::Mac) -> ast::Mac {
+        fold::noop_fold_mac(mac, self)
+    }
+}
+
+pub struct Expander {
+    loop_depth: usize,
+}
+
+impl Expander {
+    pub fn new() -> Expander {
+        Expander { loop_depth: 0 }
+    }
+
+    fn is_in_loop(&self) -> bool {
+        self.loop_depth != 0
+    }
+
+    fn fold_sub_expr(&mut self, e: Expr) -> P<Expr> {
+        P(fold::noop_fold_expr(e, self))
+    }
+
+    fn construct_expr(&mut self, node: ExprKind, id: ast::NodeId, span: Span,
+                        attrs: ast::ThinVec<ast::Attribute>) -> P<Expr> {
+        P(Expr {
+            node: node,
+            id: self.new_id(id),
+            span: self.new_span(span),
+            attrs: fold::fold_attrs(attrs.into(), self).into(),
+        })
+    }
+}
+
+impl fold::Folder for Expander {
+    fn fold_expr(&mut self, e: P<Expr>) -> P<Expr> {
+        if !e.contains_transition(self.is_in_loop()) {
+            return e;
+        }
+
+        let expr = e.unwrap();
+
+        match expr.node {
+            ExprKind::ForLoop(pat, expr, loop_block, label) => {
+                self.fold_sub_expr(desugar_for_loop(pat, expr, loop_block, label))
+            }
+            ExprKind::IfLet(pat, expr, then_block, else_block) => {
+                self.fold_sub_expr(desugar_if_let(pat, expr, then_block, else_block))
+            }
+            ExprKind::WhileLet(pat, expr, then_block, label) => {
+                self.fold_sub_expr(desugar_while_let(pat, expr, then_block, label))
+            }
+            ExprKind::While(cond, body, opt_ident) => {
+                self.loop_depth += 1;
+                let node = ExprKind::While(self.fold_expr(cond),
+                                    self.fold_block(body),
+                                    opt_ident.map(|label| respan(self.new_span(label.span),
+                                                                self.fold_ident(label.node))));
+                self.loop_depth -= 1;
+                self.construct_expr(node, expr.id, expr.span, expr.attrs)
+            }
+            ExprKind::Loop(body, opt_ident) => {
+                self.loop_depth += 1;
+                let node = ExprKind::Loop(self.fold_block(body),
+                                    opt_ident.map(|label| respan(self.new_span(label.span),
+                                                                self.fold_ident(label.node))));
+                self.loop_depth -= 1;
+                self.construct_expr(node, expr.id, expr.span, expr.attrs)
+            }
+            _ => P(expr),
+        }
+    }
+
+    fn fold_mac(&mut self, mac: ast::Mac) -> ast::Mac {
+        fold::noop_fold_mac(mac, self)
+    }
+}
+
+
+fn desugar_for_loop(pat: P<Pat>, expr: P<Expr>, loop_block: P<Block>, label: Option<SpannedIdent>) -> Expr {
+    // Desugar a for loop into:
+    //
+    // {
+    //     let mut iter = ::std::iter::IntoIterator::into_iter($expr);
+    //     'label: loop {
+    //         match iter.next() {
+    //             ::std::option::Option::Some($pat) => $loop_block,
+    //             ::std::option::Option::None => break,
+    //         }
+    //     }
+    // }
+
+    let builder = AstBuilder::new().span(expr.span);
+
+    // ::std::iter::IntoIterator::into_iter($expr)
+    let into_iter = builder.expr().call()
+        .path()
+            .global()
+            .ids(&["std", "iter", "IntoIterator", "into_iter"])
+            .build()
+        .with_arg(expr.clone())
+        .build();
+
+    // iter.next()
+    let iter_next = builder.expr().method_call("next")
+        .id("__stateful_iter")
+        .build();
+
+    // ::std::option::Option::Some($pat)
+    let some_pat = builder.pat().enum_()
+        .global().ids(&["std", "option", "Option", "Some"]).build()
+        .pat().build(pat.clone())
+        .build();
+
+    // $some_pat => $loop_block
+    let some_arm = builder.arm()
+        .with_pat(some_pat)
+        .body().build_block(loop_block.clone());
+
+    // ::std::option::Option::None
+    let none_pat = builder.pat().path()
+        .global().ids(&["std", "option", "Option", "None"]).build();
+
+    // $none_pat => break,
+    let none_arm = builder.arm()
+        .with_pat(none_pat)
+        .body().break_();
+
+    // match $iter_next() {
+    //     Some($pat) => $block,
+    //     None => break,
+    // }
+    let match_expr = builder.expr()
+        .match_().build(iter_next)
+        .with_arm(some_arm)
+        .with_arm(none_arm)
+        .build();
+
+    // `loop { $match_expr; };`
+    let mut loop_builder = builder.expr().loop_();
+
+    if let Some(label) = label {
+        loop_builder = loop_builder.label(label.node);
+    }
+
+    let loop_ = loop_builder.block()
+        .stmt().build_expr(match_expr)
+        .build();
+
+    // `let mut iter = $into_iter;`
+    let iter = builder.stmt()
+        .let_().mut_id("__stateful_iter")
+        .build_expr(into_iter);
+
+    // {
+    //     $into_iter;
+    //     $loop;
+    // }
+    builder.expr().block()
+        .with_stmt(iter)
+        .stmt().build_expr(loop_)
+        .build().unwrap()
+}
+
+fn desugar_if_let(pat: P<Pat>, expr: P<Expr>, then_block: P<Block>, else_block: Option<P<Expr>>) -> Expr {
+    // Desugar an if-let:
+    //
+    // match $expr {
+    //     $pat => $then_block,
+    //     _ => $else_block,
+    // }
+
+    let builder = AstBuilder::new().span(expr.span);
+
+    // $then_pat => $then_block
+    let then_arm = builder.arm()
+        .with_pat(pat.clone())
+        .body().build_block(then_block.clone());
+
+    // _ => $else_block
+    let else_arm = match else_block {
+        Some(ref else_block) => builder.arm().pat().wild().body().build(else_block.clone()),
+        None => builder.arm().pat().wild().body().unit(),
+    };
+
+    // match $iter_next() {
+    //     $pat => $then_block,
+    //     _ => #else_block,
+    // }
+    builder.expr()
+        .match_().build(expr.clone())
+        .with_arm(then_arm)
+        .with_arm(else_arm)
+        .build().unwrap()
+}
+
+fn desugar_while_let(pat: P<Pat>, expr: P<Expr>, then_block: P<Block>, label: Option<SpannedIdent>) -> Expr {
+    // Desugar an while-let:
+    //
+    // 'label: loop {
+    //     match $expr {
+    //         $pat => $body_block,
+    //         _ => break,
+    //     }
+    // }
+
+    let builder = AstBuilder::new().span(expr.span);
+
+    // $pat => $then_block
+    let then_arm = builder.arm()
+        .with_pat(pat.clone())
+        .body().build_block(then_block.clone());
+
+    // _ => break
+    let else_arm = builder.arm()
+        .pat().wild()
+        .body().break_();
+
+    // match $expr {
+    //     $then_arm,
+    //     $else_arm,
+    // }
+    let match_expr = builder.expr()
+        .match_().build(expr.clone())
+        .with_arm(then_arm)
+        .with_arm(else_arm)
+        .build();
+
+    // `'$label: loop { $match_expr; };`
+    let mut loop_builder = builder.expr().loop_();
+
+    if let Some(label) = label {
+        loop_builder = loop_builder.label(label.node);
+    }
+
+    loop_builder.block()
+        .stmt().build_expr(match_expr)
+        .build().unwrap()
+}
